877fbfa8b5b730095063b0b007c50e930cd5cef4
Moritz Shore <moritzshore@gmail.com>
2023-02-02 12:56:09 -0500

    Create run_soft_cal.R
    starting work on the soft-calibration functions

---
+++ R/run_soft_cal.R
@@ -0,0 +1,32 @@
+
+
+
+library(data.table) # fread
+library(dplyr) # filter, %>%, slice, ranking/row_number
+library(tidyr) # unite
+
+
+# read the wb_aa file
+basin_wb_aa = fread(
+  "C:/Users/NIBIO/Documents/GitLab/optain-swat/SWAT_softcal/swatplus_rev60_demo/basin_wb_aa.txt",
+  fill = TRUE
+)
+
+# The following is only required due to the poor formatting of the 
+# SWAT output file...
+# change the column names to those of the second row in the text file. 
+colnames(basin_wb_aa) <- basin_wb_aa %>% slice(2) %>% unlist(., use.names = F)
+# add placeholder names for the last three columns which did not get a name
+colnames(basin_wb_aa)[c(47:49)] <- c("x", "y", "z")
+# remove rows 1 to 3, as they dont contain any real data
+basin_wb_aa <- basin_wb_aa %>% filter(!row_number() %in% c(1:3))
+# Unite the last three rows into one string (as they were intended to be)
+# the new column will be named description and describes what the softcal
+# algorithm did in that step. (So its probably important).
+# the united columns are separated by a space (trailing space exists now)
+basin_wb_aa = tidyr::unite(data = basin_wb_aa,
+                           col = description,
+                           c(47:49),
+                           sep = " ") 
+
+# Now back to work:
